3f80934a074b9c587c885336be3604ee87e0c748
andrejanchevski <andrej.anchevski@sorsix.com>
2020-09-15 03:35:53 +0200

    authorization added

--- src/main/java/eshop/mk/controller/OrderController.java
+++ src/main/java/eshop/mk/controller/OrderController.java
@@ -3,6 +3,7 @@ package eshop.mk.controller;
 
 import eshop.mk.model.modelRequests.CreateOrderFromPIRequest;
 import eshop.mk.service.OrderService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -19,11 +20,13 @@ public class OrderController {
     }
 
 
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
     @PostMapping("/createOrderFromProduct")
     private void createOrderFromProduct(@RequestBody CreateOrderFromPIRequest createOrderFromPIRequest){
         this.orderService.createOrderFromProductItem(createOrderFromPIRequest);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
     @PostMapping("/createOrderFromCart")
     private void createOrderFromCart(@RequestParam UUID cartId){
         this.orderService.createOrderFromCart(cartId);
